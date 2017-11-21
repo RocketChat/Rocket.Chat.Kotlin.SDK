56a53e7ff0a4d4a803cf9afd211bb7e5357e51db
Leonardo Aramaki <leoaramaki@gmail.com>
2017-11-21 01:07:04 -0200

    Add missing fields to User model

--- common/src/main/kotlin/chat/rocket/common/model/User.kt
+++ common/src/main/kotlin/chat/rocket/common/model/User.kt
@@ -4,5 +4,11 @@ import com.squareup.moshi.Json
 
 data class User(
         @Json(name = "_id") val id: String,
-        override val username: String?
+        override val username: String?,
+        val status: BaseUser.Status,
+        val statusConnection: BaseUser.Status,
+        val utcOffset: Int,
+        val active: Boolean,
+        val success: Boolean,
+        val emails: List<Email>?
 ) : BaseUser
