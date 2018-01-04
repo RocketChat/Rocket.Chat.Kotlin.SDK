d5d174f094087d45bc0586916b948e69b6654851
Leonardo Aramaki <leoaramaki@gmail.com>
2018-01-04 21:57:26 -0200

    Implement push.token DELETE request

--- core/src/main/kotlin/chat/rocket/core/internal/rest/Push.kt
+++ core/src/main/kotlin/chat/rocket/core/internal/rest/Push.kt
@@ -1,8 +1,10 @@
 package chat.rocket.core.internal.rest
 
+import chat.rocket.common.model.BaseResult
 import chat.rocket.core.RocketChatClient
 import chat.rocket.core.internal.RestResult
 import chat.rocket.core.internal.model.PushRegistrationPayload
+import chat.rocket.core.internal.model.PushUnregistrationPayload
 import chat.rocket.core.model.PushToken
 import com.squareup.moshi.Types
 import okhttp3.MediaType
@@ -21,4 +23,18 @@ suspend fun RocketChatClient.registerPushToken(token: String) {
 
     val type = Types.newParameterizedType(RestResult::class.java, PushToken::class.java)
     handleRestCall<RestResult<PushToken>>(request, type).result()
+}
+
+suspend fun RocketChatClient.unregisterPushToken(token: String) {
+    val payload = PushUnregistrationPayload(token)
+    val adapter = moshi.adapter(PushUnregistrationPayload::class.java)
+
+    val payloadBody = adapter.toJson(payload)
+    val contentType = MediaType.parse("application/json; charset=utf-8")
+    val body = RequestBody.create(contentType, payloadBody)
+
+    val httpUrl = requestUrl(restUrl, "push.token").build()
+    val request = requestBuilder(httpUrl).delete(body).build()
+
+    handleRestCall<BaseResult>(request, BaseResult::class.java)
 }
