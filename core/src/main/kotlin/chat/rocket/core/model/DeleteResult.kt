4e5fa14cf7e58ce35d515a427b51b8a81baa1167
Leonardo Aramaki <leoaramaki@gmail.com>
2018-01-24 22:14:25 -0200

    Fix tests

--- core/src/main/kotlin/chat/rocket/core/model/DeleteResult.kt
+++ core/src/main/kotlin/chat/rocket/core/model/DeleteResult.kt
@@ -1,6 +1,5 @@
 package chat.rocket.core.model
 
-import chat.rocket.common.internal.ISO8601Date
 import com.squareup.moshi.Json
 import se.ansman.kotshi.JsonDefaultValueBoolean
 import se.ansman.kotshi.JsonSerializable
@@ -8,7 +7,7 @@ import se.ansman.kotshi.JsonSerializable
 @JsonSerializable
 data class DeleteResult(
         @Json(name = "_id") val id: String,
-        @Json(name = "ts") @ISO8601Date val timestamp: Long,
+        @Json(name = "ts") val timestamp: Long?,
         @JsonDefaultValueBoolean(false)
         val success: Boolean
 )
