998de7587bf301bcf95cb5a2223e5241ed3c30f1
Lucio Maciel <luciofm@gmail.com>
2018-02-20 13:00:56 -0300

    Add a lastModified property, for backward compatibility

--- core/src/main/kotlin/chat/rocket/core/internal/model/Subscription.kt
+++ core/src/main/kotlin/chat/rocket/core/internal/model/Subscription.kt
@@ -11,27 +11,30 @@ import se.ansman.kotshi.JsonSerializable
 
 @JsonSerializable
 data class Subscription(
-        @Json(name = "rid") val roomId: String,
-        @Json(name = "_id") override val id: String,
-        @Json(name = "t") override val type: RoomType,
-        @Json(name = "u") override val user: SimpleUser?,
-        val name: String,
-        @Json(name = "fname") override val fullName: String?,
-        @Json(name = "ro") override val readonly: Boolean? = false,
-        @Json(name = "ts") @ISO8601Date val timestamp: Long?,
-        @Json(name = "ls") @ISO8601Date val lastSeen: Long?,
-        @Json(name = "_updatedAt") @ISO8601Date override val updatedAt: Long?,
-        @Json(name = "default")
-        @JsonDefaultValueBoolean(false)
-        val isDefault: Boolean,
-        @JsonDefaultValueBoolean(false)
-        val open: Boolean,
-        @JsonDefaultValueBoolean(false)
-        val alert: Boolean,
-        @JsonDefaultValueLong(0)
-        val unread: Long,
-        @JsonDefaultValueLong(0)
-        val userMentions: Long?,
-        @JsonDefaultValueLong(0)
-        val groupMentions: Long?
-) : BaseRoom
+    @Json(name = "rid") val roomId: String,
+    @Json(name = "_id") override val id: String,
+    @Json(name = "t") override val type: RoomType,
+    @Json(name = "u") override val user: SimpleUser?,
+    val name: String,
+    @Json(name = "fname") override val fullName: String?,
+    @Json(name = "ro") override val readonly: Boolean? = false,
+    @Json(name = "ts") @ISO8601Date val timestamp: Long?,
+    @Json(name = "ls") @ISO8601Date val lastSeen: Long?,
+    @Json(name = "_updatedAt") @ISO8601Date override val updatedAt: Long?,
+    @Json(name = "default")
+    @JsonDefaultValueBoolean(false)
+    val isDefault: Boolean,
+    @JsonDefaultValueBoolean(false)
+    val open: Boolean,
+    @JsonDefaultValueBoolean(false)
+    val alert: Boolean,
+    @JsonDefaultValueLong(0)
+    val unread: Long,
+    @JsonDefaultValueLong(0)
+    val userMentions: Long?,
+    @JsonDefaultValueLong(0)
+    val groupMentions: Long?
+) : BaseRoom {
+    val lastModified: Long?
+        get() = lastSeen
+}
