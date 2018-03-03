6aba93014348776ab27c15fa0ca334cd21588ba7
Leonardo Aramaki <leoaramaki@gmail.com>
2018-03-03 16:56:12 -0300

    Add spotlight rest call

---
+++ core/src/main/kotlin/chat/rocket/core/internal/rest/Spotlight.kt
@@ -0,0 +1,15 @@
+package chat.rocket.core.internal.rest
+
+import chat.rocket.core.RocketChatClient
+import chat.rocket.core.model.SpotlightResult
+import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.withContext
+
+suspend fun RocketChatClient.spotlight(query: String): SpotlightResult = withContext(CommonPool) {
+    val httpUrl = requestUrl(restUrl, "spotlight")
+            .addQueryParameter("query", query)
+            .build()
+
+    val request = requestBuilder(httpUrl).get().build()
+    return@withContext handleRestCall<SpotlightResult>(request, SpotlightResult::class.java)
+}
