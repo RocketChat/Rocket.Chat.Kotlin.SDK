ad187049afb177ddef77bddadf89bb1d62519805
Leonardo Aramaki <leoaramaki@gmail.com>
2018-03-07 20:41:00 -0300

    Implement call to user.roles REST endpoint

---
+++ core/src/main/kotlin/chat/rocket/core/model/UserRole.kt
@@ -0,0 +1,11 @@
+package chat.rocket.core.model
+
+import com.squareup.moshi.Json
+import se.ansman.kotshi.JsonSerializable
+
+@JsonSerializable
+data class UserRole(
+        @Json(name = "_id") val id: String,
+        val username: String,
+        val roles: List<String>
+)
