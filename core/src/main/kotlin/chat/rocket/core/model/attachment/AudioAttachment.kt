62deddc4831180fcd4a3adc54badfdb219362f90
Lucio Maciel <luciofm@gmail.com>
2018-03-13 15:35:47 -0300

    Fixes to support Guggy gifs attachments

--- core/src/main/kotlin/chat/rocket/core/model/attachment/AudioAttachment.kt
+++ core/src/main/kotlin/chat/rocket/core/model/attachment/AudioAttachment.kt
@@ -6,15 +6,15 @@ import se.ansman.kotshi.JsonSerializable
 data class AudioAttachment(override val title: String?,
                            override val description: String?,
                            @Json(name = "title_link") override val titleLink: String?,
-                           @Json(name = "title_link_download") override val titleLinkDownload: Boolean,
+                           @Json(name = "title_link_download") override val titleLinkDownload: Boolean?,
                            @Json(name = "audio_url") private val audioUrl: String,
-                           @Json(name = "audio_type") private val audioType: String,
-                           @Json(name = "audio_url") private val audioSize: Long
+                           @Json(name = "audio_type") private val audioType: String?,
+                           @Json(name = "audio_url") private val audioSize: Long?
 ) : FileAttachment {
     override val url: String
         get() = audioUrl
-    override val size: Long
+    override val size: Long?
         get() = audioSize
-    override val type: String
+    override val type: String?
         get() = audioType
 }
