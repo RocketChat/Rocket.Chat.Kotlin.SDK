c303c682fde148825d34a417255eac5ca40b859a
Filipe de Lima Brito <filipedelimabrito@gmail.com>
2018-03-13 16:25:52 -0300

    Fix sign up payload.

--- core/src/main/kotlin/chat/rocket/core/internal/model/SignUpPayload.kt
+++ core/src/main/kotlin/chat/rocket/core/internal/model/SignUpPayload.kt
@@ -1,9 +1,12 @@
 package chat.rocket.core.internal.model
 
+import com.squareup.moshi.Json
 import se.ansman.kotshi.JsonSerializable
 
 @JsonSerializable
-class SignUpPayload(val username: String?,
-                    val email: String?,
-                    val password: String?,
-                    val name: String?)
+class SignUpPayload(
+        val username: String?,
+        val email: String?,
+        @Json(name = "pass") val password: String?,
+        val name: String?
+)
