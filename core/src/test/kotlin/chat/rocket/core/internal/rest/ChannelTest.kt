5f7ab5382822a89b458a15e9b9f5a9ea4b6271ae
aniketsingh03 <aniketsingh.sachan@gmail.com>
2018-03-14 05:43:39 +0530

    completes tests for creating new channel

--- core/src/test/kotlin/chat/rocket/core/internal/rest/ChannelTest.kt
+++ core/src/test/kotlin/chat/rocket/core/internal/rest/ChannelTest.kt
@@ -1,5 +1,7 @@
 package chat.rocket.core.internal.rest
 
+import chat.rocket.common.RocketChatAuthException
+import chat.rocket.common.RocketChatException
 import chat.rocket.common.model.RoomType
 import chat.rocket.common.model.Token
 import chat.rocket.common.util.PlatformLogger
@@ -8,6 +10,7 @@ import chat.rocket.core.TokenRepository
 import io.fabric8.mockwebserver.DefaultMockServer
 import kotlinx.coroutines.experimental.runBlocking
 import okhttp3.OkHttpClient
+import org.hamcrest.CoreMatchers.instanceOf
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.After
 import org.junit.Before
@@ -51,14 +54,52 @@ class ChannelTest {
                 .post()
                 .withPath("/api/v1/channels.create")
                 .andReturn(200, CREATE_CHANNEL_SUCCESS)
+
         runBlocking {
             val createChannel = sut.createChannel(roomType = RoomType.CHANNEL, name = "duplicate", usersList = listOf("aniket03"), readOnly = false)
             assertThat(createChannel.status, isEqualTo(true))
         }
     }
 
+    @Test
+    fun `createChannel() should fail as a result of duplicate channel`() {
+        mockServer.expect()
+                .post()
+                .withPath("/api/v1/channels.create")
+                .andReturn(400, FAIL_DUPLICATE_CHANNEL)
+
+        runBlocking {
+            try {
+                val createChannel = sut.createChannel(roomType = RoomType.CHANNEL, name = "elf", usersList = listOf("aniket03"), readOnly = false)
+                throw RocketChatException("unreachable code")
+            } catch (ex: Exception) {
+                assertThat(ex.message, isEqualTo("duplicate_channel"))
+            }
+        }
+
+    }
+
+    @Test
+    fun `createChannel() should fail with RocketChatAuthException if not logged in`() {
+        mockServer.expect()
+                .post()
+                .withPath("/api/v1/channels.create")
+                .andReturn(401, MUST_BE_LOGGED_ERROR)
+                .once()
+
+        runBlocking {
+            try {
+                sut.createChannel(roomType = RoomType.CHANNEL, name = "elf", usersList = listOf("aniket03"), readOnly = false)
+                throw RocketChatException("unreachable code")
+            } catch (ex: Exception) {
+                assertThat(ex, isEqualTo(instanceOf(RocketChatAuthException::class.java)))
+                assertThat(ex.message, isEqualTo("Unauthorized"))
+            }
+        }
+    }
+
     @After
-    fun shutdown(){
+    fun shutdown() {
         mockServer.shutdown()
     }
 }
