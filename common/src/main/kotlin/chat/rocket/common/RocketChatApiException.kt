bf2b358aad9aa1f6e77eb8d98974e078302a106d
Lucio Maciel <luciofm@gmail.com>
2018-04-05 14:25:58 -0300

    Add url info to Exceptions, also catch all exceptions when parsing an error response

--- common/src/main/kotlin/chat/rocket/common/RocketChatApiException.kt
+++ common/src/main/kotlin/chat/rocket/common/RocketChatApiException.kt
@@ -3,5 +3,6 @@ package chat.rocket.common
 class RocketChatApiException(
         val errorType: String,
         message: String,
-        cause: Throwable? = null
-) : RocketChatException(message, cause)
+        cause: Throwable? = null,
+        url: String? = null
+) : RocketChatException(message, cause, url)
--- common/src/main/kotlin/chat/rocket/common/RocketChatException.kt
+++ common/src/main/kotlin/chat/rocket/common/RocketChatException.kt
@@ -1,3 +1,7 @@
 package chat.rocket.common
 
-open class RocketChatException(message: String, cause: Throwable? = null) : RuntimeException(message, cause)
+open class RocketChatException(message: String, cause: Throwable? = null, private val url: String? = null) : RuntimeException(message, cause) {
+    override fun toString(): String {
+        return "${super.toString()}(url='$url')"
+    }
+}
