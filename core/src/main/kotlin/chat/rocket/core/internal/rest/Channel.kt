4725351da47aaa1a0dc0d581d85634aef1132922
Aniket Singh <aniketsingh.sachan@gmail.com>
2018-05-04 23:55:15 +0530

    fixes error while creating new channel

--- core/src/main/kotlin/chat/rocket/core/internal/rest/Channel.kt
+++ core/src/main/kotlin/chat/rocket/core/internal/rest/Channel.kt
@@ -28,7 +28,7 @@ suspend fun RocketChatClient.createChannel(roomType: RoomType, name: String, use
 
     val url = requestUrl(restUrl, getRestApiMethodNameByRoomType(roomType, "create")).build()
 
-    val request = Request.Builder().url(url).post(body).build()
+    val request = requestBuilder(url).post(body).build()
     val type = Types.newParameterizedType(RestResult::class.java, Room::class.java)
 
     return@withContext handleRestCall<RestResult<Room>>(request, type).result()
