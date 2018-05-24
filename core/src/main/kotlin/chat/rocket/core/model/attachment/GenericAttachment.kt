8e45ebf70c926931b1f0a1717972a2d220704745
Filipe de Lima Brito <filipedelimabrito@gmail.com>
2018-05-24 08:27:57 -0300

    Adds annotation class and change variable type.

--- core/src/main/kotlin/chat/rocket/core/model/attachment/GenericAttachment.kt
+++ core/src/main/kotlin/chat/rocket/core/model/attachment/GenericAttachment.kt
@@ -1,16 +1,18 @@
 package chat.rocket.core.model.attachment
 
+import chat.rocket.common.internal.ISO8601Date
 import chat.rocket.common.model.User
 import com.squareup.moshi.Json
 import se.ansman.kotshi.JsonSerializable
 
 @JsonSerializable
 data class GenericAttachment(
-    @Json(name = "_id") val id: String?,
+    @Json(name = "_id") val id: String,
     val name: String?,
     val type: String?,
-    val user: User?,
     val size: String?,
-    val uploadedAt: String?,
-    val url: String?
+    val user: User?,
+    val url: String?,
+    @ISO8601Date
+    val uploadedAt: Long
 )
