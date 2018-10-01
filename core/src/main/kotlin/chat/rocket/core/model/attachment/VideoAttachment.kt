e8077b85c7739439d34efc38df48c8915e1fa42a
Lucio Maciel <luciofm@gmail.com>
2018-10-01 18:44:30 -0300

    More default values for models

--- core/src/main/kotlin/chat/rocket/core/model/attachment/VideoAttachment.kt
+++ core/src/main/kotlin/chat/rocket/core/model/attachment/VideoAttachment.kt
@@ -3,14 +3,14 @@ package chat.rocket.core.model.attachment
 import com.squareup.moshi.Json
 
 data class VideoAttachment(
-    override val title: String?,
-    override val description: String?,
-    override val text: String?,
-    @Json(name = "title_link") override val titleLink: String?,
-    @Json(name = "title_link_download") override val titleLinkDownload: Boolean?,
+    override val title: String? = null,
+    override val description: String? = null,
+    override val text: String? = null,
+    @Json(name = "title_link") override val titleLink: String? = null,
+    @Json(name = "title_link_download") override val titleLinkDownload: Boolean? = null,
     @Json(name = "video_url") private val videoUrl: String,
-    @Json(name = "video_type") private val videoType: String?,
-    @Json(name = "video_size") private val videoSize: Long?
+    @Json(name = "video_type") private val videoType: String? = null,
+    @Json(name = "video_size") private val videoSize: Long? = null
 ) : FileAttachment {
     override val url: String
         get() = videoUrl
