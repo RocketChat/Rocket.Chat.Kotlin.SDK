bdebcf286bf952410809afd8c38f1f28e8694378
Lucio Maciel <luciofm@gmail.com>
2018-10-03 15:18:06 -0300

    Better fields support

--- core/src/main/kotlin/chat/rocket/core/model/attachment/ColorAttachment.kt
+++ core/src/main/kotlin/chat/rocket/core/model/attachment/ColorAttachment.kt
@@ -8,7 +8,8 @@ import se.ansman.kotshi.JsonSerializable
 data class ColorAttachment(
     val color: Color,
     val text: String,
-    val fallback: String? = null
+    val fallback: String? = null,
+    val fields: List<Field>? = null
 ) : Attachment {
     override val url: String
         get() = "#$color"
@@ -16,9 +17,9 @@ data class ColorAttachment(
 
 @FallbackSealedClass(name = "Custom", fieldName = "colorValue")
 sealed class Color(val color: Int, val rawColor: String) {
-    @Json(name = "good") class Good : Color(0x35AC19, "good")
-    @Json(name = "warning") class Warning : Color(0xFCB316, "warning")
-    @Json(name = "danger") class Danger : Color(0xD30230, "danger")
+    @Json(name = "good") class Good : Color(0x35AC19, "0x35AC19")
+    @Json(name = "warning") class Warning : Color(0xFCB316, "0xFCB316")
+    @Json(name = "danger") class Danger : Color(0xD30230, "0xD30230")
     class Custom(private val colorValue: String) : Color(parseColor(colorValue), colorValue)
 
     override fun toString(): String {
@@ -26,7 +27,13 @@ sealed class Color(val color: Int, val rawColor: String) {
     }
 }
 
-private const val DEFAULT_COLOR = 0xA0A0A0
+private const val DEFAULT_COLOR_INT = 0xA0A0A0
+private const val DEFAULT_COLOR_STR = "0xA0A0A0"
+val DEFAULT_COLOR = Color.Custom(DEFAULT_COLOR_STR)
+
+fun String?.asColorInt() = parseColor(this ?: DEFAULT_COLOR_STR)
+fun String?.asColor() = Color.Custom(this ?: DEFAULT_COLOR_STR)
+
 private fun parseColor(rawColor: String): Int {
     return if (rawColor.startsWith('#')) {
         var color = rawColor.substring(1).toLong(16)
@@ -34,10 +41,10 @@ private fun parseColor(rawColor: String): Int {
             // Set the alpha value
             color = color or -0x1000000
         } else if (rawColor.length != 9) {
-            color = DEFAULT_COLOR.toLong()
+            color = DEFAULT_COLOR_INT.toLong()
         }
         color.toInt()
     } else {
-        DEFAULT_COLOR
+        DEFAULT_COLOR_INT
     }
 }
