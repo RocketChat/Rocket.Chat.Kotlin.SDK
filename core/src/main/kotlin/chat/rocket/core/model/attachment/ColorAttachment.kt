c1d791034115bc68bcc9a49c735b61c4695cf1e3
Lucio Maciel <luciofm@gmail.com>
2018-10-04 08:08:52 -0300

    Fixes for Color attachment

--- core/src/main/kotlin/chat/rocket/core/model/attachment/ColorAttachment.kt
+++ core/src/main/kotlin/chat/rocket/core/model/attachment/ColorAttachment.kt
@@ -17,9 +17,9 @@ data class ColorAttachment(
 
 @FallbackSealedClass(name = "Custom", fieldName = "colorValue")
 sealed class Color(val color: Int, val rawColor: String) {
-    @Json(name = "good") class Good : Color(0x35AC19, "0x35AC19")
-    @Json(name = "warning") class Warning : Color(0xFCB316, "0xFCB316")
-    @Json(name = "danger") class Danger : Color(0xD30230, "0xD30230")
+    @Json(name = "good") class Good : Color(parseColor("#35AC19"), "#35AC19")
+    @Json(name = "warning") class Warning : Color(parseColor("#FCB316"), "#FCB316")
+    @Json(name = "danger") class Danger : Color(parseColor("#D30230"), "#D30230")
     class Custom(private val colorValue: String) : Color(parseColor(colorValue), colorValue)
 
     override fun toString(): String {
@@ -28,19 +28,23 @@ sealed class Color(val color: Int, val rawColor: String) {
 }
 
 private const val DEFAULT_COLOR_INT = 0xA0A0A0
-private const val DEFAULT_COLOR_STR = "0xA0A0A0"
+const val DEFAULT_COLOR_STR = "#A0A0A0"
 val DEFAULT_COLOR = Color.Custom(DEFAULT_COLOR_STR)
 
 fun String?.asColorInt() = parseColor(this ?: DEFAULT_COLOR_STR)
 fun String?.asColor() = Color.Custom(this ?: DEFAULT_COLOR_STR)
 
-private fun parseColor(rawColor: String): Int {
+private fun parseColor(toParseColor: String): Int {
+    var rawColor = toParseColor.toUpperCase()
+    if (rawColor.startsWith("0X")) {
+        rawColor = "#${rawColor.drop(2)}"
+    }
     return if (rawColor.startsWith('#')) {
-        var color = rawColor.substring(1).toLong(16)
-        if (rawColor.length == 7) {
+        var color = toParseColor.substring(1).toLong(16)
+        if (toParseColor.length == 7) {
             // Set the alpha value
             color = color or -0x1000000
-        } else if (rawColor.length != 9) {
+        } else if (toParseColor.length != 9) {
             color = DEFAULT_COLOR_INT.toLong()
         }
         color.toInt()
